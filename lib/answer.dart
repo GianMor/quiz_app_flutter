c1d1830af38973a3575e5c0287090a1ad8e87747
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-15 15:49:55 +0200

    create answer widget

---
+++ lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        textColor: Colors.black,
+        child: Text("Answer 1"),
+        onPressed: selectHandler,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
